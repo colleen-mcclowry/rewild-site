9337e9acb66347709903b223cfe1e6cdc2a8968c
Colleen McClowry <cmcclowry@gmail.com>
2026-03-16 21:11:44 -0500

    Add ginkgo leaf favicon

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import type { AppProps } from "next/app";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="shortcut icon" href="/favicon.svg" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
